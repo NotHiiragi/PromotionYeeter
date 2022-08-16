336d6ec3ef3462c994ce93432825ecddd57b2efa
Vilmer Johansson <Wilmer7889@gmail.com>
2022-08-16 15:24:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Promotion Yeeter
+
+Yeets the Music Pack Promotion Button/Banner from the main menu
+
+## Credits
+
+* [zoller27osu](https://github.com/zoller27osu), [Sc2ad](https://github.com/Sc2ad) and [jakibaki](https://github.com/jakibaki) - [beatsaber-hook](https://github.com/sc2ad/beatsaber-hook)
+* [raftario](https://github.com/raftario)
+* [Lauriethefish](https://github.com/Lauriethefish), [danrouse](https://github.com/danrouse) and [Bobby Shmurner](https://github.com/BobbyShmurner) for [this template](https://github.com/Lauriethefish/quest-mod-template)
---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include "main.hpp"
+#include "GlobalNamespace/MainMenuViewController.hpp"
+#include "UnityEngine/UI/Button.hpp"
+#include "GlobalNamespace/MusicPackPromoBanner.hpp"
+#include "UnityEngine/GameObject.hpp"
+
+static ModInfo modInfo; // Stores the ID and version of our mod, and is sent to the modloader upon startup
+
+// Loads the config from disk using our modInfo, then returns it for use
+// other config tools such as config-utils don't use this config, so it can be removed if those are in use
+Configuration& getConfig() {
+    static Configuration config(modInfo);
+    return config;
+}
+
+// Returns a logger, useful for printing debug messages
+Logger& getLogger() {
+    static Logger* logger = new Logger(modInfo);
+    return *logger;
+}
+
+MAKE_HOOK_MATCH(NoMorePromo, &GlobalNamespace::MainMenuViewController::DidActivate, void, GlobalNamespace::MainMenuViewController
+*self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
+
+    NoMorePromo(self, firstActivation, addedToHierarchy, screenSystemEnabling); 
+    
+    GlobalNamespace::MusicPackPromoBanner *PromotionButton = self->musicPackPromoBanner;
+    UnityEngine::GameObject *gameObject = PromotionButton->get_gameObject();
+    
+    gameObject->SetActive(false);
+}
+
+// Called at the early stages of game loading
+extern "C" void setup(ModInfo& info) {
+    info.id = MOD_ID;
+    info.version = VERSION;
+    modInfo = info;
+	
+    getConfig().Load();
+    getLogger().info("Completed setup!");
+}
+
+// Called later on in the game loading - a good time to install function hooks
+extern "C" void load() {
+    il2cpp_functions::Init();
+
+    getLogger().info("Installing hooks...");
+    // Install our hooks (1 defined)
+    INSTALL_HOOK(getLogger(), NoMorePromo);
+    getLogger().info("Installed all hooks!");
+}
